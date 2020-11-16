8924c5e3a47836013e1993588b3059dfd9a957d3
GH15ADF <synnoack@gmail.com>
2020-11-15 21:05:44 -0500

    Added meeting status and func params

--- simdata.py
+++ simdata.py
@@ -13,7 +13,6 @@ class sim:
                             "in progress"]
         self.resp_statuses_ctr = 0
         self.resp_statuses = ["Accepted",
-                                "Canceled",
                                 "None",
                                 "Not Responded",
                                 "Organizer",
@@ -23,21 +22,47 @@ class sim:
                         "     white space                 ",
                         "LONG -- >Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
                         "specials `!@#$%^&*()_+-=[]\{}|;':\",./<>?"]
+        self.meeting_statuses_ctr = 0
+        self.meeting_statuses = ["None", "Canceled"]
+
         self.SIM_DATA_TIME = "in progress"
-        self.appt_data = {"start":time.time(),"subject": "","responseStatus" : ""}
+        self.appt_data = {"start":time.time(),"subject": "","responseStatus" : "", "meeting_status" : ""}
 
     def print(self):
         print(self.appt_data)
 
-    def get_sim_data(self):
+    def get_sim_data(self, subject=None, resp_stat=None, meet_stat = None, ttime=None):
         # get the current time to start
         sim_test_time = time.time()
-        self.appt_data["subject"] = self.subjects[self.subjects_ctr % len(self.subjects)]
-        self.appt_data["responseStatus"] = self.resp_statuses[self.resp_statuses_ctr % len(self.resp_statuses)]
-        self.SIM_DATA_TIME = self.start_times[self.start_times_ctr % len(self.start_times)]
-        self.subjects_ctr += 1
-        self.start_times_ctr += 1
-        self.resp_statuses_ctr += 1
+
+        # set the subject
+        if subject==None:
+            self.appt_data["subject"] = self.subjects[self.subjects_ctr % len(self.subjects)]
+            self.subjects_ctr += 1
+        else:
+            self.appt_data["subject"] = subject
+        
+        # set response status
+        if resp_stat == None:
+            self.appt_data["responseStatus"] = self.resp_statuses[self.resp_statuses_ctr % len(self.resp_statuses)]
+            self.resp_statuses_ctr += 1
+        else:
+            self.appt_data["responseStatus"] = self.resp_statuses[self.resp_statuses.index(resp_stat)]
+        
+        # set time period
+        if ttime == None:
+            self.SIM_DATA_TIME = self.start_times[self.start_times_ctr % len(self.start_times)]
+            self.start_times_ctr += 1
+        else:
+            self.SIM_DATA_TIME = self.start_times[self.start_times.index(ttime)]       
+             
+        # set meeting status
+        if meet_stat == None:
+            self.appt_data["meeting_status"] = self.meeting_statuses[self.meeting_statuses_ctr % len(self.meeting_statuses)]
+            self.meeting_statuses_ctr += 1
+        else:
+            # i = self.meeting_statuses.index(meet_stat)
+            self.appt_data["meeting_status"] = self.meeting_statuses[self.meeting_statuses.index(meet_stat)]
 
         if self.SIM_DATA_TIME == ">1 day":
             self.appt_data["start"] = sim_test_time + (24 + 1)*60*60
